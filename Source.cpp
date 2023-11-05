a54b3190fe7e006f3e805dfcdbae205759e3af99
Max Goryunov <maximthehedgehog@yandex.ru>
2023-11-05 16:56:26 +0300

    #1 add test script

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "XmlResource.h"
 
 int main() {
+	std::unique_ptr<XmlResource> xml = XmlResource::create();
+	std::string in("input.txt");
+	xml->load(in);
+	xml->print();
+	std::string out("out.txt");
+	xml->save(out);
 	return 0;
 }
