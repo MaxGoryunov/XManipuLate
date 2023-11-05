fa831a593817a7e01eaaee2abce07317e55ef4a7
Max Goryunov <maximthehedgehog@yandex.ru>
2023-11-05 15:42:15 +0300

    #1 add load method to XmlTree

--- XmlTree.cpp
+++ XmlTree.cpp
@@ -1,11 +1,15 @@
 #include "XmlTree.h"
 #include <iostream>
+#include <stack>
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::ifstream;
 using std::ofstream;
+using std::stack;
+using std::move;
+using std::make_unique;
 
 void XmlTree::print() {
 	cout << this->head->asText() << endl;
@@ -21,7 +25,26 @@ void XmlTree::save(string& file) {
 }
 
 void XmlTree::load(ifstream& file) {
-
+	string line;
+	stack<up<Tag>> st;
+	while (getline(file, line)) {
+		if (line[1] != '/') {
+			up<Tag> cur = make_unique<Tag>(
+				line.substr(1, line.find('>') - 1),
+				stoi(line.substr(line.find('=') + 1))
+			);
+			st.push(move(cur));
+		}
+		else {
+			if (st.size() == 1) {
+				break;
+			}
+			up<Tag> child = move(st.top());
+			st.pop();
+			st.top()->push(move(child));
+		}
+	}
+	this->head = move(st.top());
 }
 
 void XmlTree::load(string& file) {
