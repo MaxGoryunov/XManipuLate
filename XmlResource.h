446f66f50d9aaaacd707b23162e3e9d66f16223a
Max Goryunov <maximthehedgehog@yandex.ru>
2023-11-06 22:47:44 +0300

    add const modifier to const methods

--- XmlResource.h
+++ XmlResource.h
@@ -11,8 +11,8 @@ private:
 	XmlResource() : tree(std::make_unique<XmlTree>()) {};
 public:
 	static std::unique_ptr<XmlResource> create();
-	void save(std::string& path) { this->tree->save(path); }
-	void load(std::string& path) { this->tree->load(path); }
+	void save(std::string& path) const { this->tree->save(path); }
+	void load(std::string& path) const { this->tree->load(path); }
 	void print() { this->tree->print(); }
 	Iterator find(std::string const& name);
 	Iterator find(int data);
